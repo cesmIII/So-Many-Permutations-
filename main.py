a6caed095430c4bac334b39e25ec9ebcc347994e
cesmIII <carlosedusm@gmail.com>
2026-06-18 21:40:18 -0300

    works till aabb

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
-from random import sample
-from math import factorial as fact
+from math import factorial as ftl
+from math import prod
+from collections import Counter as ctr
 
-def permutations(s: str):
-    # Brute force solution
+def no_repeat(string: str):
+    """Function for non repeating permutations"""
     chars = []
-    while len(set(chars)) != fact(len(s)):
-        shuffled_list = sample(s, len(s))
-        chars.append("".join(shuffled_list))
+    while len(set(chars)) != ftl(len(string)):
+        for char in range(len(string)):
+            if char == 0:
+                swap_s = string[char + 1:] + string[char]
+                chars.append(swap_s)
+            elif char < len(string) - 1:
+                swap_s = string[:char] + string[char + 1:] + string[char]
+                chars.append(swap_s)
+            else:
+                chars.append(string[char] + string[:char])
+        string = string[1:] + string[0]
     return list(set(chars))
 
+def repeating(string: str):
+    """Function for repeating permutations"""
+    ltrs = dict(ctr(string)).values()
+    perms = int(ftl(len(string)) / prod([ftl(i) for i in ltrs]))
+    chars = []
+    while len(set(chars)) != perms:
+        for char in range(len(string)):
+            if char == 0:
+                swap_s = string[char + 1:] + string[char]
+                chars.append(swap_s)
+            elif char < len(string) - 1:
+                swap_s = string[:char] + string[char + 1:] + string[char]
+                chars.append(swap_s)
+            else:
+                chars.append(string[char] + string[:char])
+        string = string[1:] + string[0]
+    return list(set(chars))
+    
 
-perm = "abc"
+def permutations(s: str):
+    if len(set(s)) == len(s):
+        return no_repeat(s)
+    else:
+        return repeating(s)
+
+
+perm = 'aaabbb'
 print(permutations(perm))
