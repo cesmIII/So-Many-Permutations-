2f089d48719bba43213d3e44a8ae01d2a58fcf9e
cesmIII <carlosedusm@gmail.com>
2026-06-21 05:13:15 -0300

    Itertools version

--- main.py
+++ main.py
@@ -1,48 +1,10 @@
-from math import factorial as ftl
-from math import prod
-from collections import Counter as ctr
+from itertools import permutations
 
-def no_repeat(string: str):
-    """Function for non repeating permutations"""
-    chars = []
-    while len(set(chars)) != ftl(len(string)):
-        for char in range(len(string)):
-            if char == 0:
-                swap_s = string[char + 1:] + string[char]
-                chars.append(swap_s)
-            elif char < len(string) - 1:
-                swap_s = string[:char] + string[char + 1:] + string[char]
-                chars.append(swap_s)
-            else:
-                chars.append(string[char] + string[:char])
-        string = string[1:] + string[0]
-    return list(set(chars))
-
-def repeating(string: str):
-    """Function for repeating permutations"""
-    ltrs = dict(ctr(string)).values()
-    perms = int(ftl(len(string)) / prod([ftl(i) for i in ltrs]))
-    chars = []
-    while len(set(chars)) != perms:
-        for char in range(len(string)):
-            if char == 0:
-                swap_s = string[char + 1:] + string[char]
-                chars.append(swap_s)
-            elif char < len(string) - 1:
-                swap_s = string[:char] + string[char + 1:] + string[char]
-                chars.append(swap_s)
-            else:
-                chars.append(string[char] + string[:char])
-        string = string[1:] + string[0]
-    return list(set(chars))
-    
-
-def permutations(s: str):
-    if len(set(s)) == len(s):
-        return no_repeat(s)
-    else:
-        return repeating(s)
+def perms(string: str):
+    """After hours of trying different things I discovered that python has a function that already do what I want"""
+    return list(set(["".join(p) for p in permutations(string)]))
 
 
-perm = 'aaabbb'
-print(permutations(perm))
+
+perm = 'abcde'
+print(perms(perm))
