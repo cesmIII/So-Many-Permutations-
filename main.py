c61495b878d08dfdb2b1692cb56dafcc746c23ba
cesmIII <carlosedusm@gmail.com>
2026-06-22 22:58:15 -0300

    heap's algorithm version

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from itertools import permutations
+def perms(arr: str) -> list:
+    arr_lst = list(arr)
+    results = []
 
-def perms(string: str):
-    """After hours of trying different things I discovered that python has a function that already do what I want"""
-    return list(set(["".join(p) for p in permutations(string)]))
+    def perm(sz: int):
+        if sz == 1:
+            # Base case: store a copy of the current permutation
+            results.append(list(arr_lst))
+            return
+
+        for i in range(sz):
+            # If we are on the last iteration, skip swapping
+            perm(sz - 1)
+
+            if i < sz - 1:
+                if sz % 2 == 0:
+                    # Even sz: swap the current index i with the last element
+                    arr_lst[i], arr_lst[sz - 1] = arr_lst[sz - 1], arr_lst[i]
+                else:
+                    # Odd sz: swap 0-th element with last element
+                    arr_lst[0], arr_lst[sz - 1] = arr_lst[sz - 1], arr_lst[0]
+
+    (perm(len(arr)))
+    return list(set(["".join(array) for array in results]))
 
 
-
-perm = 'abcde'
-print(perms(perm))
+string = 'aabc'
+print(str(perms(string)))
